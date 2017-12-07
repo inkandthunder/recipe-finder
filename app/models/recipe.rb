8cebe480298e1457408ec87e2c2c45146adda1b6
Scott DeHaan <scottdehaan@gmail.com>
2017-12-07 17:22:44 -0600

    Adding pg for production Heroku

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,9 +4,6 @@ class Recipe
     key_value = ENV['FOOD2FORK_KEY']
     hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
     base_uri "http://#{hostport}/api"
-	#base_uri 'https://food2fork.com/about/api'
-    #default_params fields: "photoUrl,description", q: "search"
-    #default_params key: ENV["FOOD2FORK_KEY"]
     default_params key: key_value
 	format :json
 
